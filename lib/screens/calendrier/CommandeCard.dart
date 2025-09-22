29ae9f65abbacbaee8ca05279416f95ac8bd0b6d
Salma Sedrati <salmasedrati18@gmail.com>
2025-09-22 19:03:34 +0200

    calendrier

--- lib/screens/calendrier/CommandeCard.dart
+++ lib/screens/calendrier/CommandeCard.dart
@@ -7,29 +7,46 @@ class CommandeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: const Color(0xFF2C2C2C),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              commande.typeCommande.toUpperCase(),
-              style: const TextStyle(
-                  color: Color(0xFFFFC107), fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              "${commande.nomClient} | ${commande.salle} | ${commande.nombreTables} tables",
-              style: const TextStyle(color: Colors.white),
-            ),
-            const SizedBox(height: 4),
-            Text(
-              commande.date,
-              style: const TextStyle(color: Colors.white70),
-            ),
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Container(
+          color: Colors.white, // même couleur que CommandeSwipeItem
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // infos de la commande
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      commande.typeCommande.toUpperCase(),
+                      style: const TextStyle(
+                        color: Color(0xFFFFC107),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      "${commande.nomClient} | ${commande.salle} | ${commande.nombreTables} tables",
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              // date
+              Text(
+                "${commande.date.substring(8, 10)}/${commande.date.substring(5, 7)}",
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
